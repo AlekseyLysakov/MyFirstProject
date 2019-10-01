f1c80f3ab3eff812fbe6ce350d7847e26a5754c0
AlekseyLysakov <49598387+AlekseyLysakov@users.noreply.github.com>
2019-10-02 00:45:34 +0300

    add code details

--- Average1.java
+++ Average1.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Average1 {
+
+    public static void ave(int a, int b){
+        int k = (a+b)/2;
+        System.out.println(k);
+    }
+
+    public static void main (String[] args){
+
+        ave(5,3);
+    }
 }
+
+
+
--- BankDeposit.java
+++ BankDeposit.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BankDeposit {
-}
+
+    public static void main(String[] args) {
+
+        Scanner deposit = new Scanner(System.in);
+        System.out.print("Введите сумму вклада в гривне: ");
+        int depositAmount1 = deposit.nextInt();
+
+        double depositAmount = depositAmount1;
+
+        Scanner percentage = new Scanner(System.in);
+        System.out.print("Введите ставку годового процента: ");
+        int annualPercentage1 = percentage.nextInt();
+
+        double annualPercentage = annualPercentage1;
+
+        Scanner period = new Scanner(System.in);
+        System.out.print("Введите период вклада в годах: ");
+        int periodYear = period.nextInt();
+
+        double a = (1.0 + (annualPercentage / 100.0));
+        double b = annualPercentage / 100.0;
+
+        for (int i = 1; i <= periodYear; i++) {
+            double percentPerYear = depositAmount*b;
+            System.out.println("Начисленные проценты за " + i + " год: " + percentPerYear);
+            depositAmount *= a;
+            System.out.println("Накопленная сумма денег за " + i + " год: " + depositAmount);
+
+
+        }
+
+            }
+    }
